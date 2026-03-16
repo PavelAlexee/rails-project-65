8c507c05a6e8b0c607369c0a4df3d9e09a04a635
PavelAlexee <pavelps41980@gmail.com>
2026-03-16 22:34:13 +0300

    fixed for deploy7

--- test/controllers/web/users_controller_test.rb
+++ test/controllers/web/users_controller_test.rb
@@ -2,7 +2,21 @@ require "test_helper"
 
 class Web::UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get profile" do
-    get web_users_profile_url
+    user = users(:john)
+
+    sign_in(user)
+
+    assert signed_in?, "User should be signed in"
+    assert_equal user.id, session[:user_id], "Session should contain user_id"
+
+    get profile_path
+
     assert_response :success
+  end
+
+  test "should redirect to root if not signed in" do
+    get profile_path
+    assert_response :redirect
+    assert_redirected_to root_path
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,39 +6,34 @@ OmniAuth.config.test_mode = true
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
 
 class ActionDispatch::IntegrationTest
-  def sign_in(user, options = {})
-    auth_hash = {
+  def sign_in(user)
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
       provider: "github",
-      uid: "12345",
+      uid: user.github_uid,
       info: {
         email: user.email,
         name: user.name
       }
-    }
+    )
 
-    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash::InfoHash.new(auth_hash)
+    get callback_auth_path("github")
 
-    get callback_auth_url("github")
+    follow_redirect!
+
+    puts "After sign_in - session user_id: #{session[:user_id]}" if ENV["DEBUG"]
   end
 
   def signed_in?
-    session[:user_id].present? && current_user.present?
+    session[:user_id].present?
   end
 
   def current_user
-    return @current_user if defined?(@current_user)
-
-    @current_user = User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
