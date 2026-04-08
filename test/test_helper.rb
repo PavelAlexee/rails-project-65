db128ec734de9fe97b7969c3b33f513305a670c5
PavelAlexee <pavelps41980@gmail.com>
2026-04-08 21:46:43 +0300

    fixed30

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,16 +13,29 @@ module ActiveSupport
   end
 end
 
-class ActionDispatch::IntegrationTest
-  setup do
-    host! 'localhost'
-  end
-
+ActionDispatch::IntegrationTest.class_eval do
   def sign_in(user)
-    session[:user_id] = user.id
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+      provider: 'github',
+      uid: user.github_uid,
+      info: {
+        email: user.email,
+        name: user.name
+      }
+    )
+
+    get callback_auth_path('github')
+
+    follow_redirect!
+
+    puts "After sign_in - session user_id: #{session[:user_id]}" if ENV['DEBUG']
   end
 
-  def sign_out
-    session[:user_id] = nil
+  def signed_in?
+    session[:user_id].present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 end
